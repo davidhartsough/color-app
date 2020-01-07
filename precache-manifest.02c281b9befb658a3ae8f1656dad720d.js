2ff17a8bb67610adc79acedcf55f13f238dd51c5
David Hartsough <hartsoughdavid@gmail.com>
2020-01-07 14:33:37 -0700

    Updates

---
+++ precache-manifest.02c281b9befb658a3ae8f1656dad720d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a0a520b6bd9cb6bb3bc613de8b96b0",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "0c9127b2066659180d67",
+    "url": "/color-app/static/css/main.3d1fe69f.chunk.css"
+  },
+  {
+    "revision": "52f46fb0329afa28c2f3",
+    "url": "/color-app/static/js/2.ab3176cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/color-app/static/js/2.ab3176cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c9127b2066659180d67",
+    "url": "/color-app/static/js/main.a1686145.chunk.js"
+  },
+  {
+    "revision": "cfb04b0fcd91a27581e4",
+    "url": "/color-app/static/js/runtime-main.bff2fbe7.js"
+  }
+]);
